ce294686f0cbe26f929597a198df27f06243ff72
razon1494 <razon1494@gmail.com>
2022-05-16 18:50:53 +0600

    test

--- src/views/components/home/ProductsArray.tsx
+++ src/views/components/home/ProductsArray.tsx
@@ -7,6 +7,7 @@ export const ProductsArray = ({ products }: { products: IProduct[] }) => {
       {products.map((product: IProduct) => (
         <ProductCard key={product._id} product={product}></ProductCard>
       ))}
+      <h1>Hey Baby</h1>
     </div>
   );
 };
