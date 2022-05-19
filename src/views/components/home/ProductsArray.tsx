67960b59d5d192863191b467ebfd715cbf14cd5d
razon1494 <razon1494@gmail.com>
2022-05-20 00:06:52 +0600

    test

--- src/views/components/home/ProductsArray.tsx
+++ src/views/components/home/ProductsArray.tsx
@@ -22,7 +22,6 @@ export const ProductsArray = ({ products }: { products: IProduct[] }) => {
       <h1 className="13"></h1>
       <h1 className="14"></h1>
       <h1 className="15"></h1>
-      <h1 className="16"></h1>
     </div>
   );
 };
