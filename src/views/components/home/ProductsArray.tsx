32946632201713137dc32b2654ce94f189c2863f
razon1494 <razon1494@gmail.com>
2022-05-19 23:33:13 +0600

    test

--- src/views/components/home/ProductsArray.tsx
+++ src/views/components/home/ProductsArray.tsx
@@ -7,7 +7,6 @@ export const ProductsArray = ({ products }: { products: IProduct[] }) => {
       {products.map((product: IProduct) => (
         <ProductCard key={product._id} product={product}></ProductCard>
       ))}
-      <h1 className="1"></h1>
     </div>
   );
 };
