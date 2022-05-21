001a9c8828f82ad11d408c840a5386138d195679
razon1494 <razon1494@gmail.com>
2022-05-21 22:06:41 +0600

    test

--- src/views/pages/home/Home.tsx
+++ src/views/pages/home/Home.tsx
@@ -37,7 +37,6 @@ const Home = () => {
         <h1 className="17"></h1>
         <h1 className="18"></h1>
         <h1 className="19"></h1>
-        <h1 className="20"></h1>
       </div>
       <Increment></Increment>
     </main>
