7d4b10376a9a04f2e3e0bb97d24f969d22e1db5d
razon1494 <razon1494@gmail.com>
2022-05-21 22:06:16 +0600

    test

--- src/hooks/useAPI.ts
+++ src/hooks/useAPI.ts
@@ -1,10 +1,22 @@
+import { useEffect, useState } from "react";
+import ProductService from "services/Product.Service";
+type StatusType = "idle" | "pending" | "success" | "error";
 const useAPI = () => {
+  /*   useEffect((service) => {
+    ProductService.getAllProducts()
+      .then((data) => setProducts(data))
+      .catch((err: string) => console.log(err));
+  }, []); */
   /*   const [products, setProducts] = useState<[] | IProduct[]>([]);
   useEffect(() => {
     ProductService.getAllProducts()
       .then((data) => setProducts(data))
       .catch((err: string) => console.log(err));
   }, []); */
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const [status, setStatus] = useState("idle");
+
   return {};
 };
 
--- src/views/pages/home/Home.tsx
+++ src/views/pages/home/Home.tsx
@@ -18,6 +18,27 @@ const Home = () => {
       </div>
       <div className="products">
         <ProductsArray products={products}></ProductsArray>
+        <h1 className="1"></h1>
+        <h1 className="2"></h1>
+        <h1 className="3"></h1>
+        <h1 className="4"></h1>
+        <h1 className="5"></h1>
+        <h1 className="6"></h1>
+        <h1 className="7"></h1>
+        <h1 className="8"></h1>
+        <h1 className="9"></h1>
+        <h1 className="10"></h1>
+        <h1 className="11"></h1>
+        <h1 className="12"></h1>
+        <h1 className="13"></h1>
+        <h1 className="14"></h1>
+        <h1 className="15"></h1>
+        <h1 className="16"></h1>
+        <h1 className="17"></h1>
+        <h1 className="18"></h1>
+        <h1 className="19"></h1>
+        <h1 className="20"></h1>
+        <h1 className="21"></h1>
       </div>
       <Increment></Increment>
     </main>
